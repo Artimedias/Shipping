427bc4853d365f33e3d825ea5fd74c83bacd3e3e
Artimedias <brettroper22@gmail.com>
2022-07-25 19:21:59 -0700

    Made the submissions for orders much more robust, can handle all the details now

--- Shipping/Controllers/HomeController.cs
+++ Shipping/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Shipping.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 namespace Shipping.Controllers
+
 {
   public class HomeController : Controller
   {
@@ -11,11 +15,29 @@ namespace Shipping.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/shipOne")]
-    public ActionResult Shipping(string Order, string Distributor)
+    public ActionResult Shipping(string Order1, string Order2, string Order3, string Distributor, string Description, string Date)
         {
+            
        ShippingVarible newOrder = new ShippingVarible();
-      newOrder.Order = Order;
+            Console.WriteLine(Order1);
+        if (Order1 != null)
+            { 
+                            newOrder.Order = Order1;
+                Console.WriteLine(Order1);
+            }
+                if (Order2 != null)
+            { 
+                            newOrder.Order = Order2;
+                Console.WriteLine(Order2);
+            }
+                        if (Order3 != null)
+            { 
+                            newOrder.Order = Order3;
+                Console.WriteLine(Order3);
+            }
             newOrder.Distributor = Distributor;
+            newOrder.Date = Date;
+            newOrder.Description = Description;
       return View(newOrder);
         }
     [Route("/form")]
--- Shipping/Models/ShippingVarible.cs
+++ Shipping/Models/ShippingVarible.cs
@@ -1,8 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
 namespace Shipping.Models
 {
     public class ShippingVarible
     {
-        public string Order { get; set; }
+        
         public string Distributor { get; set; }
+        public string Description { get; set; }
+
+        public string Time;
+        public string Goods;
+
+        public string OrderNumber { get; set; }
+
+        public string Date
+        {
+            get
+        {
+                Console.WriteLine(Time);
+                return Time;
+            }
+        set
+            {
+                Time = value;
+
+                Console.WriteLine(Time);
+            }
+        }
+        public string Order
+        {
+            get
+            {
+                return Goods;
+            }
+            set
+            {
+                Goods += (" , " + value);
+            }
+        }
     }
 }
